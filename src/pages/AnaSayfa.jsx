c653daacbd5a8725a3d4235b4f69f81f821c6be5
Anıl Teoman <anilteoman@outlook.com>
2025-03-25 13:58:11 +0300

    anasayfa css tamamlandı

--- src/pages/AnaSayfa.jsx
+++ src/pages/AnaSayfa.jsx
@@ -1,47 +1,21 @@
 import React from 'react'
-import { Link } from 'react-router-dom/cjs/react-router-dom';
-import "./anaSayfa.css";
+
+import "./AnaSayfa.css"
 
 
 
-import styled from 'styled-components';
 
-const Button = styled.button `
-
-background:#FDC913;
-padding:0.8rem 3rem;
-border:1px solid rgba(0,0,0,0);
-border-radius:1rem;
-cursor:pointer;
-font-weight: bold;
-
-
-`
-
-const Baslik = styled.h1`
-color:white;
-font-size:2rem;
-
-`
-
-const AnaSayfaYazi = styled.h2`
-color:white;
-font-size: 3.5rem;
-padding:1rem;
-`
-
-const StyledLink = styled(Link)`
-text-decoration: none;
-color:black;
-`
 
 
 
 const AnaSayfa = () => {
   return (
-    <div><Baslik>Teknolojik Yemekler</Baslik>
-    <AnaSayfaYazi>KOD ACIKTIRIR <br/> PIZZA, DOYURUR</AnaSayfaYazi>
-    <StyledLink to="./SiparisSayfa"><Button>ACIKTIM</Button></StyledLink>
+    <div className='anaSayfaContainer'>
+    <h1 className="anaSayfaLogo">Teknolojik Yemekler</h1>
+    
+    <p className="anaSayfaYazi">KOD ACIKTIRIR PIZZA, DOYURUR</p>
+    
+    <a href="./SiparisSayfa" className="aciktimBtn">ACIKTIM</a>
 
     </div>
     
